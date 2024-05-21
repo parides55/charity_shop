d8be27fcf0d3b60c528ff07f0491e3bf2eb3a072
parides55 <andreasparides5@gmail.com>
2024-05-21 16:20:14 +0100

    add and link a contact us html page

--- our_story/urls.py
+++ our_story/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.our_story, name='our_story')
+    path('', views.our_story, name='our_story'),
+    path('contact-us/', views.contact_us, name='contact_us'),
 ]
--- our_story/views.py
+++ our_story/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def our_story(request):
     return render(request, 'our_story/our-story.html',)
+
+def contact_us(request):
+    return render(request, 'our_story/contact-us.html',)
