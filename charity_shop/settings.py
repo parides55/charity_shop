363ace6caf8c47b596bea70511507989aed701e0
parides55 <andreasparides5@gmail.com>
2024-05-21 13:21:52 +0100

    update the settings.py

--- charity_shop/settings.py
+++ charity_shop/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$3h8#i%l3+^7u!2p*0@2o6mz)%2&few+k##*0!ak5-n)wx8+1='
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
