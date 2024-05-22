ad743ec1646f8a0cebac13701bde08479145e92b
parides55 <andreasparides5@gmail.com>
2024-05-22 14:41:55 +0100

    display the products from the product model

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('products/', views.products, name='products'),
+    path('products/', views.Products.as_view(), name='products'),
     path('basket/', views.basket, name='basket'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render, reverse
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic
 from django.http import HttpResponse
+from .models import Product, Basket
 
 # Create your views here.
+
+class Products(generic.ListView):
+    queryset = Product.objects.all()
+    template_name = "products/products.html"
+
 def home(request):
     return render(request, 'products/index.html',)
-
-def products(request):
-    return render(request, 'products/products.html',)
 
 def basket(request):
     return render(request, 'products/basket.html',)
