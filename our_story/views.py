4f80bd5fc2c77a521fa48210eac1ba9bfc0a0265
parides55 <andreasparides5@gmail.com>
2024-05-22 17:25:45 +0100

    display the contact us form from the ContactUs model

--- our_story/views.py
+++ our_story/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, reverse
 from django.views import generic
 from django.http import HttpResponse
 from .models import OurStory, ContactUs
+from .forms import ContactUsForm
 
 # Create your views here.
 class OurStoryListView(generic.ListView):
@@ -9,4 +10,9 @@ class OurStoryListView(generic.ListView):
     template_name = 'our_story/our-story.html'
 
 def contact_us(request):
-    return render(request, 'our_story/contact-us.html',)
+    contact_us_form = ContactUsForm()
+    return render(
+        request,
+        'our_story/contact-us.html',
+        {"contact_us_form": contact_us_form},
+        )
