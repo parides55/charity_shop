3e0b03cf1058c93f4c34a3f8828ef44b4aa154a4
parides55 <andreasparides5@gmail.com>
2024-05-24 23:48:20 +0100

    correct the url path for remove item

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('products/', views.Products.as_view(), name='products'),
     path("<slug:slug>/view_product/", views.view_product, name="view-product"),
     path('basket/', views.basket, name='basket'),
-    path('remove_item/<int:basket_id>', views.remove_item, name='remove_item')
+    path('basket/remove_item/<int:basket_id>', views.remove_item, name='remove_item')
 ]
