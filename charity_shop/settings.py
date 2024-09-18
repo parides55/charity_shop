423970c63ebb7fff80fbea6e4cfd77b5d98821bb
parides55 <andreasparides5@gmail.com>
2024-09-18 13:36:25 +0100

    add asterisk to the allowed hosts to allow custom error pages to be used

--- charity_shop/settings.py
+++ charity_shop/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '*']
 
 
 # Application definition
