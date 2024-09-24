fc1ab31cba260fa3d813ffc47b1390645fca67e4
parides55 <andreasparides5@gmail.com>
2024-09-24 09:30:23 +0100

    fix indentation to comply with pep8

--- products/views.py
+++ products/views.py
@@ -58,13 +58,16 @@ def view_product(request, slug):
             if basket_form.is_valid():
                 order_quantity = basket_form.cleaned_data['quantity']
                 # Check if product is already in the user's basket
-                existing_basket_item = Basket.objects.filter(product=product, user=request.user).first()
+                existing_basket_item = Basket.objects.filter(
+                    product=product, user=request.user).first()
 
                 if existing_basket_item:
                     # Increase the quantity of the existing item
                     existing_basket_item.quantity += order_quantity
                     existing_basket_item.save()
-                    messages.success(request, f'The quantity of {product.title} has been updated in your basket!')
+                    messages.success(
+                        request, f'The quantity of {product.title} has been '
+                        f'updated in your basket!')
                 else:
                     # Add a new item to the basket
                     order = basket_form.save(commit=False)
@@ -72,11 +75,15 @@ def view_product(request, slug):
                     order.product = product
                     order.amount = product.price
                     order.save()
-                    messages.success(request, f'{product.title} added successfully to your basket!')
+                    messages.success(
+                        request, f'{product.title} added successfully '
+                        f'to your basket!')
 
                 return redirect('basket')
             else:
-                messages.error(request, 'There was an error adding the product to Your basket. Please try again.')
+                messages.error(
+                    request, 'There was an error adding the product to '
+                    f'Your basket. Please try again.')
 
         basket_form = BasketForm()
 
@@ -125,10 +132,10 @@ def basket(request):
         total = sum(item.amount*item.quantity for item in items)
 
         context = {
-        'items': items,
-        'total': total,
-        'paypal_client_id': settings.PAYPAL_CLIENT_ID,
-    }
+            'items': items,
+            'total': total,
+            'paypal_client_id': settings.PAYPAL_CLIENT_ID,
+        }
 
         return render(request, 'products/basket.html', context)
 
@@ -144,13 +151,17 @@ def remove_item(request, basket_id):
     """
 
     try:
-        basket_item = get_object_or_404(Basket, id=basket_id, user=request.user)
+        basket_item = get_object_or_404(
+            Basket, id=basket_id, user=request.user)
         basket_item.delete()
-        messages.add_message(request, messages.ERROR, 'Item removed from your basket!')
+        messages.add_message(
+            request, messages.ERROR, 'Item removed from your basket!')
 
         return HttpResponseRedirect(reverse('basket'))
     except Exception as e:
-        messages.error(request, f'Failed to remove item because of the following error:\n{str(e)}')
+        messages.error(
+            request, f'Failed to remove item because of the following '
+            f'error:\n{str(e)}')
         return redirect('basket')
 
 
@@ -166,14 +177,18 @@ def after_payment(request):
     try:
         basket_items = Basket.objects.filter(user=request.user)
         basket_items.delete()
-        messages.success(request, 'Payment processed successfully. Your basket has been cleared.')
+        messages.success(
+            request, 'Payment processed successfully. Your basket has been '
+            f'cleared.')
 
         return render(
             request,
             'products/after_payment.html',
         )
     except Exception as e:
-        messages.error(request, f'Payment processing failed because of the following error:\n{str(e)}')
+        messages.error(
+            request, f'Payment processing failed because of the following '
+            f'error:\n{str(e)}')
         return render(request, '500.html', status=500)
 
 
@@ -203,8 +218,10 @@ def add_to_favorites(request, product_id):
         return redirect('products')
 
     except Exception as e:
-        messages.error(request, f'The following unexpected error occurred:\n{str(e)}')
+        messages.error(
+            request, f'The following unexpected error occurred:\n{str(e)}')
         return redirect('products')
+
 
 @login_required
 def favorites(request):
@@ -227,9 +244,10 @@ def favorites(request):
             'products/favorites.html',
             {'favorites': favorites, },
             )
-    
+
     except Exception as e:
-        messages.error(request, f'The following unexpected error occurred:\n{str(e)}')
+        messages.error(
+            request, f'The following unexpected error occurred:\n{str(e)}')
         return render(request, '500.html', status=500)
 
 
@@ -240,11 +258,14 @@ def remove_favorite(request, favorite_id):
     """
 
     try:
-        favorite = get_object_or_404(Favorite, id=favorite_id, user=request.user)
+        favorite = get_object_or_404(
+            Favorite, id=favorite_id, user=request.user)
         favorite.delete()
         messages.error(request, 'Item removed from favorites!')
         return HttpResponseRedirect(reverse('favorites'))
 
     except Exception as e:
-        messages.error(request, f'Failed to remove item because of the following error:\n{str(e)}')
+        messages.error(
+            request, f'Failed to remove item because of the following '
+            f'error:\n{str(e)}')
         return redirect('favorites')
